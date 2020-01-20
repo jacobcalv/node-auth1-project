1c36f773804dc5a193b5961546047f5866912b3e
jacobcalv <jacobcalvino@outlook.com>
2020-01-19 18:32:38 -0600

    refactored dbconfig

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,9 +1,5 @@
 const knex = require('knex');
 
-require('dotenv').config();
+const knexConfig = require('../knexfile.js');
 
-const knexfile = require('../knexfile.js');
-
-const environment = process.env.NODE_ENV 
-
-module.exports = knex(knexfile[environment]);
+module.exports = knex(knexConfig.development);
